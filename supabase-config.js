a6d5dc373f362650a54770c6117237f23cd03537
omiamador <omipibebb@gmail.com>
2026-09-07 11:00:45 -0600

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://hsnecccocdzpdgerebjw.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_d7FlevFw1O2CtwiS-ta8eQ_4phA8fFU';
+
+window.obraSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
